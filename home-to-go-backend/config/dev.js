afa300c7f8e3b2e3f75d5f2c6d7f666342e858e4
emoyal4 <emoyal4@gmail.com>
2023-04-16 19:04:38 +0300

    first commit

---
+++ home-to-go-backend/config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+  'dbURL': 'mongodb+srv://barlevi_atias:Bb8159075@stays.lnm3w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+}
---
+++ home-to-go-backend/config/prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+  'dbURL': 'mongodb+srv://eli:Saar130707@cluster0.uvrj7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+}
